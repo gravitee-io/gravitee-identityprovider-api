6f749a31670f1d11acb6b7c66e57b2bc59d3d473
Lorie Pisicchio <lorie.pisicchio@gmail.com>
2021-01-21 08:49:48 +0100

    feat(idp): remove useless setting

--- src/main/java/io/gravitee/identityprovider/api/social/SocialIdentityProviderConfiguration.java
+++ src/main/java/io/gravitee/identityprovider/api/social/SocialIdentityProviderConfiguration.java
@@ -41,8 +41,4 @@ public interface SocialIdentityProviderConfiguration extends IdentityProviderCon
 
     String getResponseType();
 
-    Integer getConnectTimeout();
-
-    Integer getMaxPoolSize();
-
 }
